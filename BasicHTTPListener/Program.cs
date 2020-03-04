63a312266813336e5192d90de9b15fa1cbc8fc0d
emert117 <emremert117@gmail.com>
2020-03-04 22:22:37 +0300

    Add project files.

---
+++ BasicHTTPListener/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicHTTPListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
